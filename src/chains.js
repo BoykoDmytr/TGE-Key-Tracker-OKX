d141dc366f3247e3025e89bcb2bf8481533b2300
BoykoDmytr <dimabojkoo501@gmail.com>
2026-02-01 21:47:13 +0200

    test#2

--- src/chains.js
+++ src/chains.js
@@ -1,10 +1,11 @@
 export const CHAINS = [
   { key:"eth", name:"Ethereum", type:"ws", rpc: process.env.WS_ETH, explorerTx:"https://etherscan.io/tx/" },
   { key:"arb", name:"Arbitrum", type:"ws", rpc: process.env.WS_ARB, explorerTx:"https://arbiscan.io/tx/" },
-
   { key:"base", name:"Base", type:"http", rpc: process.env.HTTP_BASE_RPC, explorerTx:"https://basescan.org/tx/" },
 
-  // ✅ додай це
+  // BSC testnet (WS)
   { key:"bsc_testnet", name:"BSC Testnet", type:"ws", rpc: process.env.WS_BSC_TESTNET, explorerTx:"https://testnet.bscscan.com/tx/" },
-  { key:"bsc", name:"BSC", type:"http", rpc: process.env.HTTP_BSC_RPC, explorerTx:"https://bscscan.com/tx/" },
+
+  // ✅ BSC mainnet (WS) — те, що ти дав
+  { key:"bsc", name:"BSC", type:"ws", rpc: process.env.WS_BSC_MAINNET, explorerTx:"https://bscscan.com/tx/" },
 ];
